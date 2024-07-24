61deb1c33b898d3ac9a864c8f38a1306bb22c599
Rishu <rishush804@gmail.com>
2024-07-24 18:14:36 +0530

    Revert "Update and rename summary_of_chat_log.py to test.py"
    This reverts commit 5038583cbb568ecc4ff59960bb57271ec1b3b0d4.

--- Blackhole/summary_of_chat_log.py
+++ Blackhole/summary_of_chat_log.py
@@ -1,6 +1,21 @@
+from ai21 import AI21Client
+from ai21.models.chat import ChatMessage
 import disnake
 from disnake.ext import commands
-import logsummary
+client = AI21Client(api_key="Insert API KEY")
+def summary(text,foc):
+    response = client.summarize.create(
+        source_type="TEXT",
+        source=text,
+        focus=foc
+    )
+    r=response.to_json()
+    i=r.find("summary")
+    i+=11
+    r=r[i:]
+    end=r.find('"')
+    r=r[:end]
+    return r.replace(r'\n',"\n")
 intents = disnake.Intents.default()
 intents.message_content = True  # Enable access to message content
 
@@ -18,6 +33,7 @@ async def get_logs(ctx,foc: str, limit: int = 100):
     logs = ""
     for message in messages:
         logs += f"{message.author.name} said  {message.content}."
+    # Split logs into chunks of 2000 characters
     await ctx.send(summary(logs,foc))
 
 bot.run("APP TOKEN")
