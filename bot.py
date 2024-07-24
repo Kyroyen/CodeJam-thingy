0c299c587714234a97f69257d2eed558b331eed0
CodeRulerNo1 <45328456+CodeRulerNo1@users.noreply.github.com>
2024-07-24 22:42:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,10 @@ async def on_ready():
     print(f'Bot connected as {bot.user}')
     
 @bot.command()
-async def summary(ctx, arg):
-    
-    await ctx.send(arg)
+async def summary(ctx, foc:foc: str, limit: int = 100):
+    messages = await ctx.channel.history(limit=limit).flatten()
+    messages.reverse()
+    logs = ""
+    for message in messages:
+        logs += f"{message.author.name} said  {message.content}."
+    await ctx.send(summary(logs,foc))
